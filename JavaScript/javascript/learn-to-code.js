34c43cbb2891515377470061d84ee7ef8f7f69fe
Gino Corrales <ginocorrales@users.noreply.github.com>
2019-02-04 21:02:48 -0600

    some full stack dev examples
    some full stack dev examples and more

---
+++ JavaScript/javascript/learn-to-code.js
@@ -0,0 +1,38 @@
+//var name = "Gino";
+//var age = "35";
+//var message = "Hi my name is " + name + " and I am " + age + " years old!!! " ;
+//
+//console.log(message);
+
+//js variables and and strings
+//var name = "Jack";
+//var age = 23;
+//var shootingScore = 45.6;
+//
+//var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+//
+//var firstName = "John";
+//var lastName = "JacobJingleHeimerSmith";
+//var dateOfBirth = "10-09-82";
+//var age = 23;
+//var profileImgUrl = 'http://coolpicks.com/johnjacob.jpg';
+//
+//var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday!" ;
+//console.log(loginWelcomeMessage);
+
+// js numbers
+//var sum = 10 + 15;
+//var sub = 15 - 10;
+//var mul = 10 * 3;
+//var div = 10 / 3;
+//var mod = 10 % 3;
+//
+//var msg = "10 / 3 = 3 with a remainder of " + mod;
+//
+//var result = 10 * ((5 + 3) - 4);
+//
+//console.log(result);
+
+// Comparisson Operators
+if (1 == 1)
+    console.log("We should see this!");
